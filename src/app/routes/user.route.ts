a2edece1959843fe13dd6299fce80736df12b111
Felipe <felipe.oliveira@urbanobank.com>
2025-05-01 17:11:09 -0300

    rotas e controller

---
+++ src/app/routes/user.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { UserController } from "../controllers/user.controller";
+// import { body } from "express-validator";
+
+const router = Router();
+
+router.post('/create', UserController.create);
+
+export default router;
