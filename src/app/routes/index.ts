23dcf8880a6ade1836c0d37d4df61adf47f40b24
Felipe <felipe.oliveira@urbanobank.com>
2025-05-07 15:50:39 -0300

    controller para auth/login

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRoutes from './user.route';
+import authRoutes from './auth.route'
 
 const router = Router();
 
 router.use('/users', userRoutes);
+router.use('/auth', authRoutes);
 
 export default router;
