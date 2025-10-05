49b24c9c884c1de2c2c066ddd78cc079fc7fad2d
Felipe Oliveira <felipeoliveira264@gmail.com>
2025-10-05 06:27:08 -0300

    retorno resource

--- src/app/service/push-notification.resource.ts
+++ src/app/service/push-notification.resource.ts
@@ -7,9 +7,11 @@ export class PushNotificationResource {
   static async registerToken(req: Request, res: Response): Promise<any> {
     try {
       const data = req.body;
+      console.log(data)
       if (data) {
         const resp = await PushNotificationService.registerTokenService(data);
-        logger.info('Push token result ->', resp);
+        logger.info('Push token result ->', resp.data);
+        return res.json({})
       }
     } catch (error: any) {
       logger.info('Falha ap persistir token para push notification', error)
