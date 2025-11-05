be786cf2aedb6fcdbbd16969f8cde1b7f481bc93
Felipe Oliveira <felipeoliveira264@gmail.com>
2025-11-05 19:53:51 -0300

    rota

--- src/app/domain/note-data/note.data.route.ts
+++ src/app/domain/note-data/note.data.route.ts
@@ -10,6 +10,6 @@ router.post(
   uploadPdf.single('file'),
   NoteDataResource.uploadPdf
 );
-router.get('/pdf/:fileName', NoteDataResource.fetchNotePdf);
+router.get('/pdfs/:fileName', NoteDataResource.fetchNotePdf);
 
 export default router;
