1ec4052cd64cb55e17555cf25f343e4bc2ae4529
Felipe Oliveira <felipeoliveira264@gmail.com>
2025-11-05 19:49:43 -0300

    ajuste tratativa retornar url pdf

--- src/app/domain/note-data/note.data.service.ts
+++ src/app/domain/note-data/note.data.service.ts
@@ -8,6 +8,9 @@ export class NoteDataService {
   static async findNoteDataService(data: any): Promise<any> {
     try {
       const resModel = await NoteDataRepository.findbyUUid(data);
+      if (resModel[0].content !== null) {
+        resModel[0].content = `${process.env.BASE_URL_ENVIRONMENT}/note-data/${resModel[0].content}`
+      }
       return resModel;
     } catch (error) {
       return error;
